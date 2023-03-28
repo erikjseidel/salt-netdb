7eab7705d4267c1fd47082f2b8dc2897e6970168
Erik Seidel <erik@znsl.us>
2023-03-28 17:23:17 +0000

    add / remove addr for interface runner

--- states/_runners/interface.py
+++ states/_runners/interface.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging, copy, json
+from ipaddress import ip_interface
 
 __virtualname__ = "interface"
 
@@ -12,13 +13,83 @@ def __virtual__():
     return __virtualname__
 
 
+def _netdb_update(data, test=True):
+    if not test:
+        netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = data, method='PUT')
+        if netdb_answer['result']:
+            netdb_answer.update({'out': data})
+            return netdb_answer
+        else:
+            return netdb_answer
+
+    return { 'result': False, 'comment': 'test=True test run', 'out': data }
+
+
 def get(device = None, interface = None):
     """
 
     """
 
     filt = [ device, None, None, interface ]
-
     netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
 
     return netdb_answer
+
+
+def new_addr(device, interface, address, ptr = None, roles = None, test = True):
+
+    filt = [ device, None, None, interface ]
+    netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
+
+    if not netdb_answer['result']:
+        return netdb_answer
+
+    try:
+        ip_interface(address)
+    except:
+        return { 'result': False, 'error': True, 'comment': 'Invalid IP address' }
+
+    data = netdb_answer['out']
+    iface = data[device]['interfaces'][interface]
+
+    if 'address' not in iface:
+        iface['address'] = {}
+    elif address in iface['address'].keys():
+        return { 'result': False, 'error': True, 'comment': 'This IP address is already assigned to %s' % interface }
+
+    iface['address'][address] = None
+
+    if ptr or roles:
+        iface['address'][address] = {}
+        iface['address'][address]['meta'] = {}
+        if ptr:
+            iface['address'][address]['meta']['dns'] = {}
+            iface['address'][address]['meta']['dns']['ptr'] = ptr
+        if roles:
+            iface['address'][address]['meta']['role'] = roles.split(',')
+
+    return _netdb_update(data, test)
+
+
+def delete_addr(device, interface, address, test = True):
+
+    filt = [ device, None, None, interface ]
+    netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
+
+    if not netdb_answer['result']:
+        return netdb_answer
+
+    try:
+        ip_interface(address)
+    except:
+        return { 'result': False, 'error': True, 'comment': 'Invalid IP address' }
+
+    data = netdb_answer['out']
+    iface = data[device]['interfaces'][interface]
+
+    if 'address' not in iface or address not in iface['address']:
+        return { 'result': False, 'error': True, 'comment': 'No such IP address assigned to %s' % interface }
+
+    iface['address'].pop(address, None)
+
+    return _netdb_update(data, test)
