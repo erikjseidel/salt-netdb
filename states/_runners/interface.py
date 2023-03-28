9b07ebd23c8450ad0b5542b987ee58e214b74fce
Erik Seidel <erik@znsl.us>
2023-03-28 17:53:02 +0000

    documentation update

--- states/_runners/interface.py
+++ states/_runners/interface.py
@@ -14,6 +14,10 @@ def __virtual__():
 
 
 def _netdb_update(data, test=True):
+    """
+    This needs to be moved to util.
+
+    """
     if not test:
         netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = data, method='PUT')
         if netdb_answer['result']:
@@ -22,22 +26,57 @@ def _netdb_update(data, test=True):
         else:
             return netdb_answer
 
-    return { 'result': False, 'comment': 'test=True test run', 'out': data }
+    return { 'result': False, 'comment': 'Test run. Database not updated.', 'out': data }
 
 
 def get(device = None, interface = None):
     """
+    Show salt managed interfaces filtered by device and/or interface name.
+
+    :param device: device to filter by.
+    :param interface: interface to filter by.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if data returned; false otherwise
+       * out: dict containing matching interfaces and their attributes
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt sin1-proxy interface.get sin3 tun376
+        salt sin1-proxy interface.get interface=tun376
 
     """
-
-    filt = [ device, None, None, interface ]
+    filt = [ device.upper(), None, None, interface ]
     netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
 
     return netdb_answer
 
 
 def new_addr(device, interface, address, ptr = None, roles = None, test = True):
+    """
+    Add an IP address to an interface.
 
+    :param device: device where interface is located
+    :param interface: interface to which the new address is to be added
+    :param ptr: dns ptr record to be associated with new address
+    :param roles: a comma separated list of address roles (currently arbitrary)
+    :param test: set true to perform netdb update (defaults to false)
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) true if successful; false otherwise
+       * out: dict containing updated interface and attributes
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt sin1-proxy interface.get sin3 tun376
+        salt sin1-proxy interface.get interface=tun376
+
+    """
+    device = device.upper()
     filt = [ device, None, None, interface ]
     netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
 
@@ -72,7 +111,27 @@ def new_addr(device, interface, address, ptr = None, roles = None, test = True):
 
 
 def delete_addr(device, interface, address, test = True):
+    """
+    Delete an IP address from an interface.
 
+    :param device: device where interface is located
+    :param interface: interface from which the new address is to be removed 
+    :param address: address to be removed 
+    :param test: set true to perform netdb update (defaults to false)
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) true if successful; false otherwise
+       * out: dict containing updated interface and attributes
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt sin1-proxy interface.get sin3 tun376
+        salt sin1-proxy interface.get interface=tun376
+
+    """
+    device = device.upper()
     filt = [ device, None, None, interface ]
     netdb_answer = __utils__['netdb_runner.request'](_COLUMN, data = filt, method='GET')
 
