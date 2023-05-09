ff024fc1cf29d366a6c9e75d133763d55d04522c
Erik Seidel <erik@znsl.us>
2023-05-09 13:53:50 +0000

    update

---
+++ states/_runners/netbox.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+import logging, copy
+from netaddr   import IPSet
+from ipaddress import ip_interface, ip_network
+from copy      import deepcopy
+
+__virtualname__ = "netbox"
+
+_NETBOX_UTIL_NAME = 'netbox'
+
+log = logging.getLogger(__file__)
+
+def __virtual__():
+    return __virtualname__
+
+
+def _call_netbox_util(function, data=None, method='GET'):
+    endpoint = _NETBOX_UTIL_NAME + '/' + function
+
+    return __utils__['netdb_runner.call_netdb_util'](endpoint, data=data, method=method)
+
+
+def generate_devices():
+    """
+    Show devices generated from Netbox source in netdb format.
+
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if data returned; false otherwise
+       * out: a dict of devices in netdb format
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.generate_devices
+
+    """
+    return _call_netbox_util('generate_devices')
+
+
+def generate_interfaces(device):
+    """
+    Show interfaces generated from Netbox source in netdb format for a device.
+
+    :param device: The device whose interfaces are to be generated.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if data returned; false otherwise
+       * out: a dict of interfaces in netdb format
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.generate_interfaces sin1
+
+    """
+    data = { "device": device }
+
+    ret = _call_netbox_util('generate_interfaces', data)
+
+    return ret
+
+
+def synchronize_devices(test=True):
+    """
+    Load devices generated from Netbox source into netdb.
+
+    :param test: Synchonize devices if false, only do a dry run if true.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if successful; false otherwise
+       * out: a dict of synchronization results
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.synchronize_devices
+
+    """
+    if not isinstance(test, bool):
+        return {"result": False, "comment": "test only accepts true or false."}
+
+    if test:
+        method = 'GET'
+    else:
+        method = 'POST'
+
+    return _call_netbox_util('synchronize_devices', method=method)
+
+
+def synchronize_interfaces(devices, test=True):
+    """
+    Load interfaces generated from Netbox source into netdb for a device.
+
+    :param device: The device whose interfaces are to be generated.
+    :param test: Synchonize interfaces if false, only do a dry run if true.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if successful; false otherwise
+       * out: a dict of synchronization results
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.synchronize_interfaces sin1
+
+    """
+    if not isinstance(test, bool):
+        return {"result": False, "comment": "test only accepts true or false."}
+
+    devs = devices.split(',')
+
+    data = { "devices": devs }
+
+    if test:
+        method = 'GET'
+    else:
+        method = 'POST'
+
+    return _call_netbox_util('synchronize_interfaces', data, method=method)
