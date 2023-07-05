73e763b23ca38eb8f6d84931a78fc3ba8c2efa1c
Erik Seidel <erik@znsl.us>
2023-07-05 16:16:05 +0000

    netbox add_pni scripts support update

--- states/_runners/netbox.py
+++ states/_runners/netbox.py
@@ -294,7 +294,7 @@ def prune_ips(test=True):
     return _call_netbox_util('prune_ips', test=test)
 
 
-def add_pni(device, interface, vlan_id=None, autogen_ips=False, ipv4=None, ipv6=None, test=True):
+def create_pni(device, interface, peer_name, cid, test=True):
     """
     Trigger the Netbox add_pni (create a new PNI) script. Creates new IPs on the
     interface as well. Sub-interfaces for VLAN tagged PNIs will be dynamically
@@ -302,7 +302,96 @@ def add_pni(device, interface, vlan_id=None, autogen_ips=False, ipv4=None, ipv6=
 
     :param device: Device where PNI is located
     :param interface: Interface name of the PNI
+    :param peer_name: Name of PNI Peer (netbox Provider)
+    :param cid: Circuit ID
+    :param test: Commit changes to Netbox if false, only do a dry run if true.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if successful; false otherwise
+       * out: a dict of script run results
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.create_pni sin1 eth5 Acme cid=ACM001 test=false
+        salt-run netbox.create_pni sin1 eth5 Acme ACM001
+
+    """
+    if not isinstance(test, bool):
+        return {"result": False, "comment": "test only accepts true or false."}
+
+    data = {
+            'device'     : device.upper(),
+            'interface'  : interface,
+            'peer_name'  : peer_name,
+            'circuit_id' : cid,
+            }
+
+    data = { k : v for k, v in data.items() if v }
+
+    return _call_netbox_util('create_pni', data=data, test=test)
+
+
+def create_bundle(device, interfaces, layer3_4=False, lacp_slow=False, test=True):
+    """
+    Trigger the Netbox add_pni (create a new PNI) script. Creates new IPs on the
+    interface as well. Sub-interfaces for VLAN tagged PNIs will be dynamically
+    created.
+
+    :param device: Device where PNI is located
+    :param interfaces: Interfaces to be included in the bundle (comma separated list)
+    :param layer3_4: True to enable layer3+4 hashing (non-standard)
+    :param lacp_slow: True to enable LACP slow rate
+    :param test: Commit changes to Netbox if false, only do a dry run if true.
+    :return: a dictionary consisting of the following keys:
+
+       * result: (bool) True if successful; false otherwise
+       * out: a dict of script run results
+
+    CLI Example::
+
+    .. code-block:: bash
+
+        salt-run netbox.create_bundle sin1 eth4,eth5 test=false
+        salt-run netbox.create_bundle sin1 eth5 layer3_4=true
+
+    """
+    if not isinstance(test, bool):
+        return {"result": False, "comment": "test only accepts true or false."}
+
+    if not isinstance(layer3_4, bool):
+        return {"result": False, "comment": "layer3_4 only accepts true or false."}
+
+    if not isinstance(lacp_slow, bool):
+        return {"result": False, "comment": "lacp_slow only accepts true or false."}
+
+    if not isinstance(interfaces, str):
+        return {"result": False, "comment": "interfaces must be a comma separated list"}
+
+    data = {
+            'device'     : device.upper(),
+            'interfaces' : interfaces.split(','),
+            'layer3_4'   : layer3_4,
+            'lacp_slow'  : lacp_slow,
+            }
+
+    data = { k : v for k, v in data.items() if v }
+
+    return _call_netbox_util('create_bundle', data=data, test=test)
+
+
+def configure_pni(device, interface, peer_asn, vlan_id=None, vcid=None, autogen_ips=False, ipv4=None, ipv6=None, test=True):
+    """
+    Trigger the Netbox add_pni (create a new PNI) script. Creates new IPs on the
+    interface as well. Sub-interfaces for VLAN tagged PNIs will be dynamically
+    created.
+
+    :param device: Device where PNI is located
+    :param interface: Interface name of the PNI
+    :param peer_asn: ASN of PNI / vPNI peer
     :param vlan_id: 802.1Q VLAN tag (if any)
+    :param vcid: "Virtual" circuit ID (for VLANs, optional)
     :param autogen_ips: Automatically assign IPv4 /31 and IPv6 /127 from PNI IP pool
     :param ipv4: Manually assign IPv4 address (not used if autogen_ips set)
     :param ipv6: Manually assign IPv6 address (not used if autogen_ips set)
@@ -316,22 +405,24 @@ def add_pni(device, interface, vlan_id=None, autogen_ips=False, ipv4=None, ipv6=
 
     .. code-block:: bash
 
-        salt-run netbox.add_pni sin1 eth5 vlan_id=650 autogen_ips=false test=false
-        salt-run netbox.add_pni sin1 eth5 ipv4=23.181.24.96/31 ipv6=2620:136:a009:df99::2/127
+        salt-run netbox.configure_pni sin1 eth5 13335 vlan_id=650 vcid=CF001 autogen_ips=false test=false
+        salt-run netbox.configure_pni sin1 eth5 13335 ipv4=23.181.24.96/31 ipv6=2620:136:a009:df99::2/127
 
     """
     if not isinstance(test, bool):
         return {"result": False, "comment": "test only accepts true or false."}
 
     data = {
-            'device'      : device.upper(),
-            'interface'   : interface,
-            'vlan_id'     : vlan_id,
-            'autogen_ips' : autogen_ips,
-            'my_ipv4'     : ipv4,
-            'my_ipv6'     : ipv6,
+            'device'             : device.upper(),
+            'interface'          : interface,
+            'vlan_id'            : vlan_id,
+            'virtual_circuit_id' : vcid,
+            'peer_asn'           : peer_asn,
+            'autogen_ips'        : autogen_ips,
+            'my_ipv4'            : ipv4,
+            'my_ipv6'            : ipv6,
             }
 
     data = { k : v for k, v in data.items() if v }
 
-    return _call_netbox_util('add_pni', data=data, test=test)
+    return _call_netbox_util('configure_pni', data=data, test=test)
